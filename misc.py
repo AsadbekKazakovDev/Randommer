c9733a302d465afff2f0681de95471db6a93c482
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-08-14 10:39:56 +0500

    misc.py->

--- misc.py
+++ misc.py
@@ -1,9 +1,12 @@
 import requests
 from randommer import Randommer
+randomer=Randommer()
 
 
 class Misc(Randommer):
-    def get_cultures(self, api_key: str) -> list:
+    def __init__(self,api_key)  :
+        self.api_key=api_key
+    def get_cultures(self) -> list:
         '''get available misc cultures
 
         Args:
@@ -12,9 +15,19 @@ class Misc(Randommer):
         Returns:
             list: list of cultures
         '''
-        pass
+        base_url='https://randommer.io'
+        url=base_url+"/Misc/Cultures/"
+        heardes={
+            "X-Api-Key":self.api_key
+        }
+        response=requests.get(url,headers=heardes)
+        if response.status_code==200:
+            data = response.json()
+        else:
+            data = {"error":"bad response"}
+        return data
     
-    def get_random_address(self, api_key: str, number: int, culture='en') -> list:
+    def get_random_address(self,number: int, culture='en') -> list:
         '''get available misc cultures
 
         Args:
@@ -25,4 +38,20 @@ class Misc(Randommer):
         Returns:
             list: random address
         '''
-        pass
+        base_url='https://randommer.io'
+        url=base_url+"/Misc/Random-Address"
+        heardes={
+            "X-Api-Key":self.api_key
+        }
+        payload={
+            "number":number
+        }
+        response=requests.get(url,params=payload,headers=heardes)
+        if response.status_code==200:
+            data = response.json()
+        else:
+            data = {"error":"bad response"}
+        return data
+key="f1ab06cd2da14928a4f4299e85162d76"
+misc=Misc(key)
+print(misc.get_random_address(1))
