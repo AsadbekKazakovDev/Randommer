d29401e66d3b52bbf556fd9b12c21157aa818f91
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-08-14 09:40:18 +0500

    finance.py->

--- finance.py
+++ finance.py
@@ -1,8 +1,10 @@
 import requests
 from randommer import Randommer
-
+randoner=Randommer()
 class Finance(Randommer):
-    def get_crypto_address_types(self, api_key: str) -> list:
+    def __init__(self,api_key):
+        self.api_key=api_key
+    def get_crypto_address_types(self) -> list:
         '''get available crypto types
 
         Args:
@@ -11,9 +13,20 @@ class Finance(Randommer):
         Returns:
             list: list of types
         '''
-        pass
+        base_url='https://randommer.io'
+        url=base_url+"/Finance/CryptoAddress/Types"
+        heardes={
+            "X-Api-Key":self.api_key
+        }
+        response=requests.get(url,headers=heardes)
 
-    def get_crypto_address(self, crypto_type: str, api_key: str) -> dict:
+        if response.status_code==200:
+            data = response.json()
+        else:
+            data = {"error":"bad response"}
+        return data
+
+    def get_crypto_address(self, crypto_type: str,) -> dict:
         '''get available crypto address
 
         Args:
@@ -23,9 +36,23 @@ class Finance(Randommer):
         Returns:
             dict: crypto address
         '''
-        pass
+        base_url='https://randommer.io'
+        url=base_url+"/Finance/CryptoAddress"
+        heardes={
+            "X-Api-Key":self.api_key
+        }
+        payload={
+            "cryptoType":crypto_type
+        }
+        response=requests.get(url,params=payload, headers=heardes)
+        if response.status_code==200:
+            data = response.json()
+        else:
+            data = {"error":"bad response"}
+        return data
 
-    def get_countries(self, api_key: str) -> list:
+
+    def get_countries(self,) -> list:
         '''get available countries
 
         Args:
@@ -34,9 +61,19 @@ class Finance(Randommer):
         Returns:
             list: crypto address
         '''
-        pass
+        base_url='https://randommer.io'
+        url=base_url+"/Finance/Countries"
+        heardes={
+            "X-Api-Key":self.api_key
+        }
+        response=requests.get(url,headers=heardes)
+        if response.status_code==200:
+            data = response.json()
+        else:
+            data = {"error":"bad response"}
+        return data
 
-    def get_iban_by_country_code(self, country_code: str, api_key: str) -> dict:
+    def get_iban_by_country_code(self, country_code: str) -> dict:
         '''get available countries
 
         Args:
@@ -46,4 +83,21 @@ class Finance(Randommer):
         Returns:
             dict: idan data
         '''
-        pass
+        base_url='https://randommer.io'
+        url=base_url+f"/Finance/Iban/{country_code}"
+        heardes={
+            "X-Api-Key":self.api_key
+        }
+        payload={
+            "countryCode ":country_code
+        }
+
+        response=requests.get(url,params=payload,headers=heardes)
+        if response.status_code==200:
+            data = response.json()
+        else:
+            data = {"error":"bad response"}
+        return data
+key="2d794c6f46094ceb96bd719c1c26c984"
+finace=Finance(key)
+print(finace.get_iban_by_country_code("SA"))
