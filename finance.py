808215d14db06e14f0010298c1754ef4e0ddd89b
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-08-15 09:29:45 +0500

    finance.py->

--- finance.py
+++ finance.py
@@ -1,10 +1,8 @@
 import requests
 from randommer import Randommer
-randoner=Randommer()
+
 class Finance(Randommer):
-    def __init__(self,api_key):
-        self.api_key=api_key
-    def get_crypto_address_types(self) -> list:
+    def get_crypto_address_types(self, api_key: str) -> list:
         '''get available crypto types
 
         Args:
@@ -13,20 +11,21 @@ class Finance(Randommer):
         Returns:
             list: list of types
         '''
-        base_url='https://randommer.io'
-        url=base_url+"/Finance/CryptoAddress/Types"
-        heardes={
-            "X-Api-Key":self.api_key
-        }
-        response=requests.get(url,headers=heardes)
+        endpoint = '/api/Card/Types/'
+        url = self.get_url() + endpoint
 
-        if response.status_code==200:
+        headers = {
+            "X-Api-Key": api_key
+        }
+        response=requests.get(f'{url}Finance/CryptoAddress/Types',headers=headers)
+        if response.status_code == 200:
             data = response.json()
         else:
-            data = {"error":"bad response"}
+            data = {"error": "Bad request."}
+        
         return data
-
-    def get_crypto_address(self, crypto_type: str,) -> dict:
+        
+    def get_crypto_address(self, crypto_type: str, api_key: str) -> dict:
         '''get available crypto address
 
         Args:
@@ -36,23 +35,22 @@ class Finance(Randommer):
         Returns:
             dict: crypto address
         '''
-        base_url='https://randommer.io'
-        url=base_url+"/Finance/CryptoAddress"
-        heardes={
-            "X-Api-Key":self.api_key
+        headers={ 
+            "X-Api-key": api_key
         }
-        payload={
-            "cryptoType":crypto_type
+        cryptoType={
+            'cryptoType' : crypto_type
         }
-        response=requests.get(url,params=payload, headers=heardes)
-        if response.status_code==200:
+        url=self.get_url()
+        response=requests.get(f'{url}Finance/CryptoAddress',headers=headers , params=cryptoType)
+        if response.status_code == 200:
             data = response.json()
         else:
-            data = {"error":"bad response"}
+            data = {"error": "Bad request."}
+        
         return data
 
-
-    def get_countries(self,) -> list:
+    def get_countries(self, api_key: str) -> list:
         '''get available countries
 
         Args:
@@ -61,19 +59,17 @@ class Finance(Randommer):
         Returns:
             list: crypto address
         '''
-        base_url='https://randommer.io'
-        url=base_url+"/Finance/Countries"
-        heardes={
-            "X-Api-Key":self.api_key
-        }
-        response=requests.get(url,headers=heardes)
-        if response.status_code==200:
+        headers={ "X-Api-key":api_key }
+        url=self.get_url()
+        response=requests.get(f'{url}Finance/Countries',headers=headers)
+        if response.status_code == 200:
             data = response.json()
         else:
-            data = {"error":"bad response"}
+            data = {"error": "Bad request."}
+        
         return data
 
-    def get_iban_by_country_code(self, country_code: str) -> dict:
+    def get_iban_by_country_code(self, country_code: str, api_key: str) -> dict:
         '''get available countries
 
         Args:
@@ -83,21 +79,21 @@ class Finance(Randommer):
         Returns:
             dict: idan data
         '''
-        base_url='https://randommer.io'
-        url=base_url+f"/Finance/Iban/{country_code}"
-        heardes={
-            "X-Api-Key":self.api_key
-        }
-        payload={
-            "countryCode ":country_code
-        }
-
-        response=requests.get(url,params=payload,headers=heardes)
-        if response.status_code==200:
+        headers={ "X-Api-key":api_key }
+        countrycode={'countrycode': country_code }
+        url=self.get_url()
+        response=requests.get(f'{url}Finance/Iban/{country_code}',headers=headers,params=countrycode)
+        if response.status_code == 200:
             data = response.json()
         else:
-            data = {"error":"bad response"}
+            data = {"error": "Bad request."}
+        
         return data
-key="2d794c6f46094ceb96bd719c1c26c984"
-finace=Finance(key)
-print(finace.get_iban_by_country_code("SA"))
+        
+
+ans=Finance()
+key = "2d794c6f46094ceb96bd719c1c26c984"
+print(ans.get_crypto_address_types(key))
+print(ans.get_crypto_address('Dash', key))
+print(ans.get_countries(key))
+print(ans.get_iban_by_country_code('AL', key))
